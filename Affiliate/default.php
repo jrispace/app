a23dee1235e5f4d210413b7a66fa0043a3be681f
jrihosting <67241401+jrihosting@users.noreply.github.com>
2020-06-29 22:28:29 -0400

    Add files via upload

---
+++ Affiliate/default.php
@@ -0,0 +1,10 @@
+<title>Earn up to 10% of money with JRiSpace</title>
+<meta http-equiv="refresh" content="20; url=https://jrispace.net/webdesign/affiliate-home/" />
+<p >
+    </br></br></br></nr>
+    <h3 style="color:green" align="center">
+    Welcome to our affiliate website.</br>
+    You are here to earn, right?</br>
+    So why waiting? <a href="https://jrispace.net/webdesign/affiliate-home/">Let Start Now</a>
+    </h3>
+</p>
---
+++ shared-hosting/default.php
@@ -0,0 +1,12 @@
+<html>
+    <header>
+        <title>JRiSpace Web Hosting</title>
+        <h2 align="center">JRiSpace Web Hosting</h2><h3 align="center">_______________►◄_______________</h3></br>
+    </header>
+    <body>
+            <iframe frameborder='0' width="100%" height="650px" src='http://jrihost.duoservers.com/hosting-order/'></iframe>
+    </body>
+    <footer>
+        <h3 align="center">Powered by <a href="http://jrihost.com" target="blank">JRiHosting</a></h3>
+    </footer>
+</html>
